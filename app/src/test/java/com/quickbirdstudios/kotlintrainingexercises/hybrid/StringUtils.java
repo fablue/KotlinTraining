9cbd529f21c53829284ddf18ccbfbb3f52ec7ecb
Malte Bucksch <malte.bucksch@quickbirdstudios.com>
2017-12-10 11:49:13 +0100

    Add tasks

--- app/src/test/java/com/quickbirdstudios/kotlintrainingexercises/hybrid/StringUtils.java
+++ app/src/test/java/com/quickbirdstudios/kotlintrainingexercises/hybrid/StringUtils.java
@@ -9,6 +9,8 @@ public final class StringUtils {
         //no instance
     }
 
+    //    TODO TASK 1 after conversion of StringUtils to Kotlin, first try the annotation @JvmStatic
+    //    TODO TASK 2 after conversion of StringUtils AND Farmer to Kotlin, convert this to a top-level extension function
     public static String screamify(String text){
         return text.toUpperCase() + "!!!";
     }
